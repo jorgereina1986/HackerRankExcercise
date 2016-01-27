a0c34b779676fd0814d5d03983321d9001d0af68
jorgereina1986 <jorgereina1986@gmail.com>
2016-01-26 20:13:33 -0500

    HackerRank practice exercise

---
+++ src/com/jorgereina/Main.java
@@ -0,0 +1,47 @@
+package com.jorgereina;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.management.StringValueExp;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        solvePuzzle(909090);
+    }
+
+    public static void solvePuzzle(long num)
+    {
+
+        Map<Character, Integer> holesMap = new HashMap<Character, Integer>();
+        holesMap.put('1', 0);
+        holesMap.put('2', 0);
+        holesMap.put('3', 0);
+        holesMap.put('4', 1);
+        holesMap.put('5', 0);
+        holesMap.put('6', 1);
+        holesMap.put('8', 2);
+        holesMap.put('9', 1);
+        holesMap.put('0', 1);
+
+        String strNum = num + "";
+        long sum = 0;
+
+        for(int i = 0; i < strNum.length(); i++)
+        {
+            char singleNum = strNum.charAt(i);
+
+            int numOfHoles = holesMap.get(singleNum);
+            sum = sum + numOfHoles;
+
+        }
+        System.out.println(sum);
+
+
+    }
+}
